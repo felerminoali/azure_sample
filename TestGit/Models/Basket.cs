9a2b6ce3ed955a2d60d655fc09ce6f5ddd20b864
felerminoali <felerminoali@gmail.com>
2017-01-22 17:55:53 +0200

    shortString functioning

--- TestGit/Models/Basket.cs
+++ TestGit/Models/Basket.cs
@@ -78,7 +78,7 @@ namespace TestGit.Models
                     //    list.Add(category.name + ": (" + shortenString(item.title, 120) + ")");
                     //}
 
-                    list.Add("item: " + item.title + " cate: " + basket[key]);
+                    list.Add("item: " + shortenString(item.title, 120) + " cate: " + basket[key]);
 
                 }
 
